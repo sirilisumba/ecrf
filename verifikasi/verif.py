95a0945add954c1f44b654f4393785032a27a4a6
Sirili Sumba <91815379+sirilisumba@users.noreply.github.com>
2025-12-01 06:55:25 +0700

    Update verif.py

--- verifikasi/verif.py
+++ verifikasi/verif.py
@@ -338,19 +338,19 @@ try:
         exit()
             
     # --- tunggu tombol VERIFY muncul ---
-    # verify_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-verify")))
-    # # scroll ke tombol dan klik
-    # driver.execute_script("arguments[0].scrollIntoView(true);", verify_btn)
-    # time.sleep(0.2)
-    # driver.execute_script("arguments[0].click();", verify_btn)
-    # print("✅ Tombol VERIFY berhasil diklik")
+    verify_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-verify")))
+    # scroll ke tombol dan klik
+    driver.execute_script("arguments[0].scrollIntoView(true);", verify_btn)
+    time.sleep(0.2)
+    driver.execute_script("arguments[0].click();", verify_btn)
+    print("✅ Tombol VERIFY berhasil diklik")
 
     # --- tunggu tombol KEMBALI muncul ---
-    back_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-close")))
-    driver.execute_script("arguments[0].scrollIntoView(true);", back_btn)
-    time.sleep(0.2)
-    driver.execute_script("arguments[0].click();", back_btn)
-    print("✅ Tombol KEMBALI berhasil diklik")
+    # back_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-close")))
+    # driver.execute_script("arguments[0].scrollIntoView(true);", back_btn)
+    # time.sleep(0.2)
+    # driver.execute_script("arguments[0].click();", back_btn)
+    # print("✅ Tombol KEMBALI berhasil diklik")
 
 
     # ---  LANJUT KE FORM BERIKUTNYA 
@@ -365,4 +365,5 @@ except KeyboardInterrupt:
 except Exception as e:
     play_error()
     print(f"❌ Error di loop utama: {e}")
+
 
