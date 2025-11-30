62b9a05a25a83fca56167eb559e542e4c522c3d7
Sirili Sumba <91815379+sirilisumba@users.noreply.github.com>
2025-12-01 06:56:56 +0700

    Update verifpasien.py

--- verifikasi/verifpasien.py
+++ verifikasi/verifpasien.py
@@ -142,19 +142,19 @@ try:
         exit()
 
     # --- tunggu tombol VERIFY muncul ---
-    # verify_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-verify")))
-    # # scroll ke tombol dan klik
-    # driver.execute_script("arguments[0].scrollIntoView(true);", verify_btn)
-    # time.sleep(0.2)
-    # driver.execute_script("arguments[0].click();", verify_btn)
-    # print("✅ Tombol VERIFY berhasil diklik")
+    verify_btn = wait_short.until(EC.element_to_be_clickable((By.ID, "btn-verify")))
+    # scroll ke tombol dan klik
+    driver.execute_script("arguments[0].scrollIntoView(true);", verify_btn)
+    time.sleep(0.2)
+    driver.execute_script("arguments[0].click();", verify_btn)
+    print("✅ Tombol VERIFY berhasil diklik")
 
     # --- tunggu tombol KEMBALI muncul ---
-    back_btn = wait_short.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn__cancel")))
-    driver.execute_script("arguments[0].scrollIntoView(true);", back_btn)
-    time.sleep(0.2)
-    driver.execute_script("arguments[0].click();", back_btn)
-    print("✅ Tombol KEMBALI berhasil diklik")
+    # back_btn = wait_short.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn__cancel")))
+    # driver.execute_script("arguments[0].scrollIntoView(true);", back_btn)
+    # time.sleep(0.2)
+    # driver.execute_script("arguments[0].click();", back_btn)
+    # print("✅ Tombol KEMBALI berhasil diklik")
 
     # ---  LANJUT KE FORM BERIKUTNYA 
     next_file = "verifvaksinasi.exe" if getattr(sys, 'frozen', False) else "verifvaksinasi.py"
@@ -169,3 +169,4 @@ except KeyboardInterrupt:
 except Exception as e:
     play_error()
     print(f"❌ Error di verifpasien: {e}")
+
